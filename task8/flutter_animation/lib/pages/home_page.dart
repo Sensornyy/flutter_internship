981937003814c4b10764dc18977634a1fbc7b132
Sensornyy <maksym.koriak@chisw.com>
2022-12-15 13:28:44 +0200

    refactor: replace Align widget with Flower widget

--- task8/flutter_animation/lib/pages/home_page.dart
+++ task8/flutter_animation/lib/pages/home_page.dart
@@ -4,6 +4,7 @@ import 'package:flutter_animation/widgets/bird.dart';
 
 import '../widgets/bee.dart';
 import '../widgets/cloud.dart';
+import '../widgets/flower.dart';
 import '../widgets/spinner.dart';
 import '../widgets/sun.dart';
 
@@ -20,100 +21,76 @@ class _HomePageState extends State<HomePage> {
     var screenHeight = MediaQuery.of(context).size.height;
 
     return Scaffold(
-        body: Stack(children: <Widget>[
-      Container(
-        height: screenHeight * 0.8,
-        color: AppColors.skyColor,
-      ),
-      const Align(alignment: Alignment.topLeft, child: Sun()),
-      const Align(
-        alignment: Alignment.topLeft,
-        child: Cloud(),
-      ),
-      const Align(alignment: Alignment.centerLeft, child: Bird()),
-      Align(
-        alignment: Alignment.bottomCenter,
-        child: Container(
-          height: screenHeight * 0.2,
-          color: AppColors.groundColor,
-        ),
-      ),
-      Align(
-          alignment: Alignment.bottomCenter,
-          child: Padding(
-            padding: const EdgeInsets.only(bottom: 55),
+      body: Stack(
+        children: <Widget>[
+          Container(
+            height: screenHeight * 0.8,
+            color: AppColors.skyColor,
+          ),
+          const Align(
+            alignment: Alignment.topLeft,
+            child: Sun(),
+          ),
+          const Align(
+            alignment: Alignment.topLeft,
+            child: Cloud(),
+          ),
+          const Align(
+            alignment: Alignment.centerLeft,
+            child: Bird(),
+          ),
+          Align(
+            alignment: Alignment.bottomCenter,
             child: Container(
-              height: screenHeight * 0.6,
-              width: 50,
-              decoration: BoxDecoration(
-                  color: AppColors.treeColor,
-                  borderRadius: BorderRadius.circular(10)),
+              height: screenHeight * 0.2,
+              color: AppColors.groundColor,
             ),
-          )),
-      const Align(
-        alignment: Alignment.center,
-        child: Spinner(),
-      ),
-      Align(
-        alignment: Alignment.bottomLeft,
-        child: Padding(
-          padding: const EdgeInsets.only(bottom: 45.0, left: 10.0),
-          child: Image.asset(
-            'assets/images/flower.png',
-            height: 40,
-            width: 40,
           ),
-        ),
-      ),
-      Align(
-        alignment: Alignment.bottomLeft,
-        child: Padding(
-          padding: const EdgeInsets.only(bottom: 65.0, left: 75.0),
-          child: Image.asset(
-            'assets/images/flower.png',
-            height: 40,
-            width: 40,
+          Align(
+              alignment: Alignment.bottomCenter,
+              child: Padding(
+                padding: const EdgeInsets.only(bottom: 55),
+                child: Container(
+                  height: screenHeight * 0.6,
+                  width: 50,
+                  decoration: BoxDecoration(
+                      color: AppColors.treeColor,
+                      borderRadius: BorderRadius.circular(10)),
+                ),
+              )),
+          const Align(
+            alignment: Alignment.center,
+            child: Spinner(),
           ),
-        ),
-      ),
-      Align(
-        alignment: Alignment.bottomCenter,
-        child: Padding(
-          padding: const EdgeInsets.only(bottom: 15.0, right: 100.0),
-          child: Image.asset(
-            'assets/images/flower.png',
-            height: 40,
-            width: 40,
+          const Flower(
+            alignment: Alignment.bottomLeft,
+            padding: EdgeInsets.only(bottom: 45.0, left: 10.0),
           ),
-        ),
-      ),
-      const Align(
-        alignment: Alignment.bottomLeft,
-        child: Padding(
-            padding: EdgeInsets.only(bottom: 105.0, left: 30.0), child: Bee()),
-      ),
-      Align(
-        alignment: Alignment.bottomCenter,
-        child: Padding(
-          padding: const EdgeInsets.only(bottom: 15.0, left: 120.0),
-          child: Image.asset(
-            'assets/images/flower.png',
-            height: 40,
-            width: 40,
+          const Flower(
+            alignment: Alignment.bottomLeft,
+            padding: EdgeInsets.only(bottom: 65.0, left: 75.0),
           ),
-        ),
-      ),
-      Align(
-        alignment: Alignment.bottomRight,
-        child: Padding(
-          padding: const EdgeInsets.only(bottom: 50.0, right: 40.0),
-          child: Image.asset(
-            'assets/images/flower.png',
-            height: 40,
-            width: 40,
+          const Flower(
+            alignment: Alignment.bottomCenter,
+            padding: EdgeInsets.only(bottom: 15.0, right: 100.0),
           ),
-        ),
+          const Align(
+            alignment: Alignment.bottomLeft,
+            child: Padding(
+              padding: EdgeInsets.only(bottom: 105.0, left: 30.0),
+              child: Bee(),
+            ),
+          ),
+          const Flower(
+            alignment: Alignment.bottomCenter,
+            padding: EdgeInsets.only(bottom: 15.0, left: 120.0),
+          ),
+          const Flower(
+            alignment: Alignment.bottomRight,
+            padding: EdgeInsets.only(bottom: 50.0, right: 40.0),
+          ),
+        ],
       ),
-    ]));
+    );
   }
 }
